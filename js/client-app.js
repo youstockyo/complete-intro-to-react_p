c2913b293287fd192831b764f836907bd37372a8
youstockyo <cjeustaquio@gmail.com>
2016-09-13 21:55:17 -0700

    introducing semistandard for testing; webpack to combine files

--- js/client-app.js
+++ js/client-app.js
@@ -1,25 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var MyTitle = require('./my-title.js');
 var div = React.DOM.div;
-var h1 = React.DOM.h1;
-
-var MyTitle = React.createClass({
-	render: function() {
-		return (
-			div(null,
-				h1({style: {color: this.props.color}}, this.props.title)
-			)
-		)
-	}
-});
 
 // Equivalent to React.createElement(MyTitle, null)
 var MyTitleFactory = React.createFactory(MyTitle);
 
 var MyFirstComponent = (
-	div(null,
-		MyTitleFactory({title: 'Props are great!', color: 'peru'}),
-		React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'rebeccapurple'}),
-		React.createElement(MyTitle, {title: 'Props are the best!', color: 'mediumaquamarine'})
-	)
+  div(null,
+    MyTitleFactory({title: 'Props are great!', color: 'peru'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'Props are the best!', color: 'mediumaquamarine'})
+  )
 );
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'));
